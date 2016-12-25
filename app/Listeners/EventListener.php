85ff5dca83e27b79c7983ac430758973309f536c
OST_MAC <ost5253@gmail.com>
2016-12-25 12:25:29 +0900

    chatting is not working

--- app/Listeners/EventListener.php
+++ app/Listeners/EventListener.php
@@ -33,18 +33,18 @@ class EventListener implements ShouldQueue
 
         Log::info("--------");
 //        Log::info($data->data->name);
-        Log::info($data->data->hash_tag."--------");
+//        Log::info($data->data->hash_tag."--------");
 
         $brain = new BrainSocketAppResponse();
-//
+
 //        $new_chat = new Chat();
 //        $new_chat->user_id = $data->data->user_id;
 //        $new_chat->hash_tag_id = $data->data->hash_tag_id;
 //        $new_chat->message = $data->data->message;
 //        $new_chat->save();
-
-
-        return $brain->message($data->data->hash_tag, [
+echo "---data--message = ". $data->data->message."\n";
+echo "HI\n";
+        return $brain->message('receive.message', [
             'name' => $data->data->name,
             'message' => $data->data->message
         ]);

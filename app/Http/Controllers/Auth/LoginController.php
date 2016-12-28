16449cbbbffd7ae5cdbd38f6b6cf4e22c0c0f47a
OST_MAC <ost5253@gmail.com>
2016-12-29 00:33:35 +0900

    auth:api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,6 +40,7 @@ class LoginController extends Controller
     public function __construct()
     {
 //        $this->middleware('guest', ['except' => 'logout']);
+        $this->middleware('auth:api');
     }
 
     public function login(Request $request)
@@ -79,7 +80,7 @@ class LoginController extends Controller
 
         $this->clearLoginAttempts($request);
 
-        
+
 
 
 

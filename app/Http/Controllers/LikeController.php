c2d9d59071005bb2438063a71202c04bf8e4dad3
OST_MAC <ost5253@gmail.com>
2017-03-04 02:11:12 +0900

    like store2

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -10,11 +10,16 @@ class LikeController extends Controller
 {
     public function store($id)
     {
-        if (Auth::guard('api')->user()) {
+        if (Auth::guard('api')->user() && Like::where('user_id', Auth::guard('api')->user()->id)
+                ->where('post_id', $id)->get()->isEmpty()
+        ) {
+
             $like = new Like();
             $like->user_id = Auth::guard('api')->user()->id;
             $like->post_id = $id;
             $like->save();
+            
+
         } else {
             return response("먼저 로그인 하세요", 402);
         }

131c537f98540f511c60f4daaea1e3188ead5def
OST <ost5253@gmail.com>
2017-03-08 16:30:58 +0900

    Hashtag ordering is done

---
+++ app/Http/Controllers/HashTagController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\HashTag;
+use Illuminate\Http\Request;
+
+class HashTagController extends Controller
+{
+    public function hashtag(Request $request)
+    {
+        $hashtags = HashTag::where('tag', 'like', $request->tag . "%")
+            ->orderby('tag')->get();
+        return response()->json($hashtags);
+    }
+}

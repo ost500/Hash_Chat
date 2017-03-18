c5d6a37a6de095b2e3bed0a03729866ffed6c0b7
OST_MAC <ost5253@gmail.com>
2017-03-18 20:06:07 +0900

    ses email

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'ost5253@gmail.com',
+        'name' => '수원대 마음의 편지',
     ],
 
     /*

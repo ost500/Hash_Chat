03c19402b82f809d53b66dff76219a44721fda17
OST_MAC <ost5253@gmail.com>
2017-07-25 00:10:00 +0900

    instagram crawling

---
+++ app/Console/Commands/InstagramCommand.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\HashTag;
+use App\Instagram;
+use App\Post;
+use App\PostHashTag;
+use App\User;
+use Exception;
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class InstagramCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawl:Instagram';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        /** @var HashTag $ssuls */
+        $tags = HashTag::get();
+        $tags->each(function (HashTag $tag) {
+            try {
+
+
+                $title = $tag->tag;
+
+                $title = str_replace(' ', '', $title);
+
+                $title = preg_replace("/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", '', $title);
+
+                echo "[Instagram]" . $title . "\n";
+
+                $media = $this->getMediaByTag(urlencode($title));
+
+                foreach ($media as $me) {
+                    foreach ($me['media']['nodes'] as $node) {
+                        if (!Instagram::where('display_src', $node['display_src'])->exists()) {
+                            DB::transaction(function () use ($node, $title, $tag) {
+                                $newInstagram = new Instagram();
+
+                                $newInstagram->display_src = $node['display_src'];
+                                $newInstagram->date = $node['date'];
+                                $newInstagram->caption = $node['caption'];
+
+
+                                $newInstagram->save();
+
+                                $newPost = new Post();
+
+                                $instaUser = User::where('name', "익명")->first();
+
+                                $newPost->user_id = $instaUser->id;
+
+                                $imagePath = public_path("image/" . $newInstagram->id);
+                                file_put_contents($imagePath, file_get_contents($node['display_src']));
+
+                                $newPost->picture = "image/" . $newInstagram->id;
+
+                                $content = $node['caption'];
+                                $newPost->message = $content;
+
+                                $newPost->save();
+
+
+                                $newPostHashTag = new PostHashTag();
+                                $newPostHashTag->post_id = $newPost->id;
+                                $newPostHashTag->hash_tag_id = $tag->id;
+                                $newPostHashTag->save();
+
+                            });
+                        }
+                    }
+
+                }
+            } catch (Exception $e) {
+                echo "[Instagram Crawling]에러가 발생했습니다 {$title}\n" . $e->getMessage() . "\n" . $e->getTraceAsString() . "\n";
+            }
+        });
+    }
+
+    public function getMediaByTag(string $name): array
+    {
+        $client = new Client([
+            'base_uri' => 'https://www.instagram.com',
+            'query' => ['__a' => 1],
+        ]);
+        $response = $client->request('GET', '/explore/tags/' . $name);
+
+        $body = json_decode($response->getBody()->getContents(), true);
+
+        return ($body);
+    }
+
+}
